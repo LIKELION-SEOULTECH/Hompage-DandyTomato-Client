8b564d18cad5c35e50e274b1fe0c8dad61a1024e
KimDaegyu <ddk3207@naver.com>
2025-06-18 22:08:05 +0900

    feat: 다크모드 전환 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import WithMockServer from './mocks/WithMockServer'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import StickerRain from './test'
 const queryClient = new QueryClient()
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/mode_toggle'
 
 export default function App() {
     const [user, setUser] = useState<{ name: string; age: number } | null>(null)
@@ -22,14 +23,18 @@ export default function App() {
 
     return (
         <>
-            <QueryClientProvider client={queryClient}></QueryClientProvider>
-            {/* <div className="p-4 text-xl">
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider>
+                    {/* <div className="p-4 text-xl">
                 <p className="user-name">이름: {user.name}</p>
                 <p>나이: {user.age}</p>
                 <WithMockServer />
             </div> */}
-            <StickerRain />
-            {/* <h1 className="text-4xl font-bold">App.tsx</h1> */}
+                    <StickerRain />
+                    <ModeToggle />
+                    {/* <h1 className="text-4xl font-bold">App.tsx</h1> */}
+                </ThemeProvider>
+            </QueryClientProvider>
         </>
     )
 }
