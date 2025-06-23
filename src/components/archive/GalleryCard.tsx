44db9184e5d41059a178619305ac1bd43ae3f1d3
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-06-23 09:23:23 +0900

    feat: GalleryCard 해쉬태그 props 추가

--- src/components/archive/GalleryCard.tsx
+++ src/components/archive/GalleryCard.tsx
@@ -1,17 +1,26 @@
 import { cn } from '@/lib/utils'
 import React, { useState } from 'react'
+import { Badge } from '../ui/badge'
 
 interface GalleryCardProps {
     className?: string
     style?: React.CSSProperties
     index?: number
+    title?: string
+    date?: string
+    description?: string
+    tag?: string[]
     // Add other props like title, imageUrl, etc. as needed
 }
 
 const GalleryCard: React.FC<GalleryCardProps> = ({
     className,
     style,
-    index
+    index,
+    title = '활동이름',
+    date = '2025.01.01',
+    description = '간단한 설명',
+    tag = ['해쉬태그']
 }) => {
     const [isHovered, setIsHovered] = useState(false)
     return (
@@ -33,12 +42,21 @@ const GalleryCard: React.FC<GalleryCardProps> = ({
                     'flex h-full w-full flex-col items-center justify-center gap-10 bg-[oklch(0%_0_0/0.2)]'
                 )}>
                 <h3 className="text-20 text-sub-seoultech-red bg-pri-white font-bold">
-                    활동이름
+                    {title}
                 </h3>
                 <p className="text-16 text-pri-whit text-center font-bold">
-                    2025
-                    <br /> 정기세션
+                    {date}
+                    <br /> {description}
                 </p>
+                <div className="flex flex-wrap gap-10">
+                    {tag.map(tag => (
+                        <Badge
+                            className="text-16 text-sub-seoultech-red border-sub-seoultech-red rounded-50 border-2 bg-transparent px-8 py-4 font-bold"
+                            variant={'default'}>
+                            #{tag}
+                        </Badge>
+                    ))}
+                </div>
             </div>
         </div>
     )
