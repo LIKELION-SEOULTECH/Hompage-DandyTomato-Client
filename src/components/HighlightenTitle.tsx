d7711757a9b04b8d216b3427a7c0638093e2b6f6
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-07-03 07:30:42 +0900

    feat: 메인페이지 sticky 애니메이션

--- src/components/HighlightenTitle.tsx
+++ src/components/HighlightenTitle.tsx
@@ -2,13 +2,16 @@ import { cn } from '@/lib/utils'
 
 export default function HighlightenTitle({
     text,
-    className
+    className,
+    ref
 }: {
     text: string
     className?: string
+    ref?: React.RefObject<HTMLDivElement>
 }) {
     return (
         <h1
+            ref={ref}
             className={cn(
                 'font-pretendard text-pri-white bg-sub-seoultech-red text-64 w-fit leading-none font-bold tracking-[-1.92px]',
                 className
